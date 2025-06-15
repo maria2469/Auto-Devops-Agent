7091350bce03da5dd6070c93958996503b5763d9
maria2469 <tomarianoor@gmail.com>
2025-06-15 14:02:41 +0500

    changes deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Auto-Devops-Agent/', // 👈 Correct location for the base path
   plugins: [
     react(),
-    tailwindcss(),
-  ],
+    tailwindcss()
+  ]
 })
