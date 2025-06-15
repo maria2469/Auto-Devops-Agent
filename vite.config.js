90f112e6e463db641e6b384c907b71b154bd9b7b
maria2469 <tomarianoor@gmail.com>
2025-06-15 15:53:04 +0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Auto-Devops-Agent/', // <-- This is critical for GitHub Pages
+  
   plugins: [react(), tailwindcss()],
-});
+})
